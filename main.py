de8f6469ca17290c27f64d0f91adb018b751eaf9
matsko1506 <matsko1506@gmail.com>
2026-05-10 18:28:59 +0200

    Discord token security

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from lynx-bot!")
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
 
+load_dotenv(".env")
+DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
 
-if __name__ == "__main__":
-    main()
+client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Connected to Discord")
+
+client.run(DISCORD_TOKEN)
+
